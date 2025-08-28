506a0ef8a0ac619020827a336624db1fb2ed5028
akturks <akturks@gmail.com>
2025-08-27 21:39:49 -0700

    fix: restore session manager check in permission validation
    - Restore check for session manager claims issuer
    - Ensures proper distinction between local and SSO users

--- server/account/account.go
+++ server/account/account.go
@@ -158,7 +158,7 @@ func toAPIAccount(name string, a settings.Account) *account.Account {
 func (s *Server) ensureHasAccountPermission(ctx context.Context, action string, account string) error {
 	id := session.GetUserIdentifier(ctx)
 
-	if id == account {
+	if id == account && session.Iss(ctx) == session.SessionManagerClaimsIssuer {
 		return nil
 	}
 	if err := s.enf.EnforceErr(ctx.Value("claims"), rbac.ResourceAccounts, action, account); err != nil {
